397233d7e8b80f9dc74d091973ca562ee9011ea9
Afgan Khalilov <halilov.lib@gmail.com>
2024-03-20 20:50:11 +0400

    Fix npm

---
+++ app/set-new/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import SetNew from "@/components/pages/SetNew";
+import {Suspense} from "react";
+
+export default function Page() {
+    return <Suspense>
+                <SetNew />
+            </Suspense>
+}
